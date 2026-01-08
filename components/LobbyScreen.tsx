1efb25bca5175d22dd3ba7b962114383e5cb075e
puleri <puleri.mp4@gmail.com>
2026-01-08 10:45:01 -0800

    added question menu button

--- components/LobbyScreen.tsx
+++ components/LobbyScreen.tsx
@@ -5,23 +5,31 @@ import UsernameForm from "./UsernameForm";
 export default function LobbyScreen() {
   return (
     <main>
-      <img className="welcome-div" src="/images/skyjo-hero-banner.png" alt="Skyjo Hero Banner"/>
-        
-      <div className="container">      
-      <h2 className="sage-eyebrow-text">GETTING STARTED</h2>
+      <img className="welcome-div" src="/images/skyjo-hero-banner.png" alt="Skyjo Hero Banner" />
 
-      <section>
-        <UsernameForm />
-      </section>
+      <div className="container">
+        <div className="flex-space-between">
+          <h2 className="sage-eyebrow-text">GETTING STARTED</h2>
+          <div className="question-mark-div">
+            <img
+              className="question-mark-icon"
+              src="/question-mark-icon.svg"
+              alt="Skyjo Instructions Menu Icon"
+            />
+          </div>
+        </div>
+        <section>
+          <UsernameForm />
+        </section>
 
-      <section className="form-card">
-        <CreateLobbyForm />
-      </section>
+        <section className="form-card">
+          <CreateLobbyForm />
+        </section>
 
-      <div className="lobby-list-section">
-        <h2 className="charcoal-eyebrow-text">LOBBIES</h2>
-        <LobbyList />
-      </div>
+        <div className="lobby-list-section">
+          <h2 className="charcoal-eyebrow-text">LOBBIES</h2>
+          <LobbyList />
+        </div>
 
       </div>
     </main>
